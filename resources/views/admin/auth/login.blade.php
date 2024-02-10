322ba15fe22f95d64abffbdf6520f7bfc3ca7a14
Amir Khalife Soltani <amirsoltani94@gmail.com>
2024-02-10 12:08:30 +0330

    mod: modify login forms appearance

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,35 +1,60 @@
-<div>
-{{--    @if (count($errors))--}}
-{{--        <ul>--}}
-{{--            @foreach($errors->all() as $error)--}}
-{{--                <li>{{ $error }}</li>--}}
-{{--            @endforeach--}}
-{{--        </ul>--}}
-{{--    @endif--}}
+@extends('template.layout')
+
+@section('content')
+    {{--    @if (count($errors))--}}
+    {{--        <ul>--}}
+    {{--            @foreach($errors->all() as $error)--}}
+    {{--                <li>{{ $error }}</li>--}}
+    {{--            @endforeach--}}
+    {{--        </ul>--}}
+    {{--    @endif--}}
+
+    <div class="relative flex flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+        <h4 class="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+            Admin Login
+        </h4>
+        <p class="block mt-1 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+            Have a good day!
+        </p>
+        <form class="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96" method="POST" action="{{ route('admin.do_login') }}">
+            @csrf
+            <div class="flex flex-col gap-6 mb-1">
+                @error('email')
+                <div class="text-red-500">{{ $message }}</div>
+                @enderror
+                <h6
+                        class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                    Your Email
+                </h6>
+                <div class="relative h-11 w-full min-w-[200px]">
+                    <input name="email" placeholder="name@mail.com"
+                           class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"/>
+                    <label
+                            class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                </div>
+
+
+                @error('password')
+                <div class="text-red-500">{{ $message }}</div>
+                @enderror
+                <h6
+                        class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                    Password
+                </h6>
+                <div class="relative h-11 w-full min-w-[200px]">
+                    <input name="password" type="password" placeholder="********"
+                           class="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"/>
+                    <label
+                            class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                </div>
+
+            </div>
+            <button class="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    type="submit">
+                Login
+            </button>
+        </form>
+    </div>
 
     <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-    <form method="POST" action="{{ route('admin.do_login') }}">
-        @csrf
-        <label for="email">Email</label>
-        <input id="email"
-               name="email"
-               type="text"
-               class="@error('email') is-invalid @enderror">
-
-        @error('email')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-
-        <label for="password">Password</label>
-        <input id="password"
-               name="password"
-               type="password"
-               class="@error('password') is-invalid @enderror">
-
-        @error('password')
-        <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-
-        <button type="submit">Login</button>
-    </form>
-</div>
+@endsection
